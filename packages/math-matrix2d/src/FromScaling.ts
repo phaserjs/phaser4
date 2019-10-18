f7af002e66722dddf33aad8e1d180e0ac71e05e9
Richard Davey <rich@photonstorm.com>
2019-10-18 19:51:33 +0100

    Update FromScaling.ts

--- packages/math-matrix2d/src/FromScaling.ts
+++ packages/math-matrix2d/src/FromScaling.ts
@@ -2,7 +2,5 @@ import { Matrix2D } from './Matrix2D';
 
 export function FromScaling (scaleX: number, scaleY: number = scaleX): Matrix2D
 {
-    const out: Matrix2D = new Matrix2D();
-
-    return out.scale(scaleX, scaleY);
+    return new Matrix2D(scaleX, 0, 0, scaleY);
 }
