d982a2b7798b11de025eb4bc8db5f9d98e3555d4
Richard Davey <rich@photonstorm.com>
2019-10-29 12:47:11 +0000

    Added UBO size cache data

--- packages/renderer-webgl2/src/GetUniform.ts
+++ packages/renderer-webgl2/src/GetUniform.ts
@@ -1,4 +1,5 @@
 import { IUniformData } from './IUniformData';
+import { IUniformSize } from './IUniformSize';
 
 const FV: string = 'fv';
 
@@ -6,51 +7,69 @@ const SAMPLER_TYPE = [ '1i', 1, Int32Array, false ];
 
 const glConstMap = {
 
-    0x8B5E: SAMPLER_TYPE,                                // SAMPLER_2D
-    0x8DC1: SAMPLER_TYPE,                                // SAMPLER_2D_ARRAY
-    0x8B62: SAMPLER_TYPE,                                // SAMPLER_2D_SHADOW
-    0x8DC4: SAMPLER_TYPE,                                // SAMPLER_2D_ARRAY_SHADOW
-    0x8DCA: SAMPLER_TYPE,                                // INT_SAMPLER_2D
-    0x8DCF: SAMPLER_TYPE,                                // INT_SAMPLER_2D_ARRAY
-    0x8DD2: SAMPLER_TYPE,                                // UNSIGNED_INT_SAMPLER_2D
-    0x8DD7: SAMPLER_TYPE,                                // UNSIGNED_INT_SAMPLER_2D_ARRAY
-    0x8B60: SAMPLER_TYPE,                                // SAMPLER_CUBE
-    0x8DCC: SAMPLER_TYPE,                                // INT_SAMPLER_CUBE
-    0x8DD4: SAMPLER_TYPE,                                // UNSIGNED_INT_SAMPLER_CUBE
-    0x8DC5: SAMPLER_TYPE,                                // SAMPLER_CUBE_SHADOW
-    0x8B5F: SAMPLER_TYPE,                                // SAMPLER_3D
-    0x8DCB: SAMPLER_TYPE,                                // INT_SAMPLER_3D
-    0x8DD3: SAMPLER_TYPE,                                // UNSIGNED_INT_SAMPLER_3D
+    0x8B5E: SAMPLER_TYPE,                                              // SAMPLER_2D
+    0x8DC1: SAMPLER_TYPE,                                              // SAMPLER_2D_ARRAY
+    0x8B62: SAMPLER_TYPE,                                              // SAMPLER_2D_SHADOW
+    0x8DC4: SAMPLER_TYPE,                                              // SAMPLER_2D_ARRAY_SHADOW
+    0x8DCA: SAMPLER_TYPE,                                              // INT_SAMPLER_2D
+    0x8DCF: SAMPLER_TYPE,                                              // INT_SAMPLER_2D_ARRAY
+    0x8DD2: SAMPLER_TYPE,                                              // UNSIGNED_INT_SAMPLER_2D
+    0x8DD7: SAMPLER_TYPE,                                              // UNSIGNED_INT_SAMPLER_2D_ARRAY
+    0x8B60: SAMPLER_TYPE,                                              // SAMPLER_CUBE
+    0x8DCC: SAMPLER_TYPE,                                              // INT_SAMPLER_CUBE
+    0x8DD4: SAMPLER_TYPE,                                              // UNSIGNED_INT_SAMPLER_CUBE
+    0x8DC5: SAMPLER_TYPE,                                              // SAMPLER_CUBE_SHADOW
+    0x8B5F: SAMPLER_TYPE,                                              // SAMPLER_3D
+    0x8DCB: SAMPLER_TYPE,                                              // INT_SAMPLER_3D
+    0x8DD3: SAMPLER_TYPE,                                              // UNSIGNED_INT_SAMPLER_3D
 
-    0x1406: [ '1f', 1, Float32Array, false ],            // FLOAT
-    0x8B50: [ '2f', 2, Float32Array, false ],            // FLOAT_VEC2
-    0x8B51: [ '3f', 3, Float32Array, false ],            // FLOAT_VEC3
-    0x8B52: [ '4f', 4, Float32Array, false ],            // FLOAT_VEC4
-    0x1404: [ '1i', 1, Int32Array, false ],              // INT
-    0x8B53: [ '2i', 2, Int32Array, false ],              // INT_VEC2
-    0x8B54: [ '3i', 3, Int32Array, false ],              // INT_VEC3
-    0x8B55: [ '4i', 4, Int32Array, false ],              // INT_VEC4
-    0x1405: [ '1ui', 1, Uint32Array, false ],            // UNSIGNED_INT
-    0x8DC6: [ '2ui', 2, Uint32Array, false ],            // UNSIGNED_INT_VEC2
-    0x8DC7: [ '3ui', 3, Uint32Array, false ],            // UNSIGNED_INT_VEC3
-    0x8DC8: [ '4ui', 4, Uint32Array, false ],            // UNSIGNED_INT_VEC4
+    0x1406: [ '1f', 1, Float32Array, false, 1, 0x1406, 1 ],            // FLOAT
+    0x8B50: [ '2f', 2, Float32Array, false, 2, 0x1406, 2 ],            // FLOAT_VEC2
+    0x8B51: [ '3f', 3, Float32Array, false, 4, 0x1406, 4 ],            // FLOAT_VEC3
+    0x8B52: [ '4f', 4, Float32Array, false, 4, 0x1406, 4 ],            // FLOAT_VEC4
 
-    0x8B5A: [ '2' + FV, 4, Float32Array, true ],         // FLOAT_MAT2
-    0x8B5B: [ '3' + FV, 9, Float32Array, true ],         // FLOAT_MAT3
-    0x8B5C: [ '4' + FV, 16, Float32Array, true ],        // FLOAT_MAT4
-    0x8B65: [ '2x3' + FV, 6, Float32Array, true],        // FLOAT_MAT2x3
-    0x8B66: [ '2x4' + FV, 8, Float32Array, true],        // FLOAT_MAT2x4
-    0x8B67: [ '3x2' + FV, 6, Float32Array, true],        // FLOAT_MAT3x2
-    0x8B68: [ '3x4' + FV, 12, Float32Array, true],       // FLOAT_MAT3x4
-    0x8B69: [ '4x2' + FV, 8, Float32Array, true],        // FLOAT_MAT4x2
-    0x8B6A: [ '4x3' + FV, 12, Float32Array, true],       // FLOAT_MAT4x3
+    0x1404: [ '1i', 1, Int32Array, false, 1, 0x1404, 1 ],              // INT
+    0x8B53: [ '2i', 2, Int32Array, false, 2, 0x1404, 2 ],              // INT_VEC2
+    0x8B54: [ '3i', 3, Int32Array, false, 4, 0x1404, 4 ],              // INT_VEC3
+    0x8B55: [ '4i', 4, Int32Array, false, 4, 0x1404, 4 ],              // INT_VEC4
 
-    0x8B56: [ '1iv', 1, Array, false ],                  // BOOL
-    0x8B57: [ '2iv', 2, Array, false ],                  // BOOL_VEC2
-    0x8B58: [ '3iv', 3, Array, false ],                  // BOOL_VEC3
-    0x8B59: [ '4iv', 4, Array, false ]                   // BOOL_VEC4
+    0x1405: [ '1ui', 1, Uint32Array, false, 1, 0x1405, 1 ],            // UNSIGNED_INT
+    0x8DC6: [ '2ui', 2, Uint32Array, false, 2, 0x1405, 2 ],            // UNSIGNED_INT_VEC2
+    0x8DC7: [ '3ui', 3, Uint32Array, false, 4, 0x1405, 4 ],            // UNSIGNED_INT_VEC3
+    0x8DC8: [ '4ui', 4, Uint32Array, false, 4, 0x1405, 4 ],            // UNSIGNED_INT_VEC4
+
+    0x8B5A: [ '2' + FV, 4, Float32Array, true, 4, 0x1406, 8 ],         // FLOAT_MAT2
+    0x8B5B: [ '3' + FV, 9, Float32Array, true, 4, 0x1406, 12 ],        // FLOAT_MAT3
+    0x8B5C: [ '4' + FV, 16, Float32Array, true, 4, 0x1406, 16 ],       // FLOAT_MAT4
+    0x8B65: [ '2x3' + FV, 6, Float32Array, true, 4, 0x1406, 8 ],       // FLOAT_MAT2x3
+    0x8B66: [ '2x4' + FV, 8, Float32Array, true, 4, 0x1406, 8 ],       // FLOAT_MAT2x4
+    0x8B67: [ '3x2' + FV, 6, Float32Array, true, 4, 0x1406, 12 ],      // FLOAT_MAT3x2
+    0x8B68: [ '3x4' + FV, 12, Float32Array, true, 4, 0x1406, 12 ],     // FLOAT_MAT3x4
+    0x8B69: [ '4x2' + FV, 8, Float32Array, true, 4, 0x1406, 16 ],      // FLOAT_MAT4x2
+    0x8B6A: [ '4x3' + FV, 12, Float32Array, true, 4, 0x1406, 16 ],     // FLOAT_MAT4x3
+
+    0x8B56: [ '1iv', 1, Array, false, 1, 0x1406, 1 ],                  // BOOL
+    0x8B57: [ '2iv', 2, Array, false, 2, 0x1406, 2 ],                  // BOOL_VEC2
+    0x8B58: [ '3iv', 3, Array, false, 4, 0x1406, 4 ],                  // BOOL_VEC3
+    0x8B59: [ '4iv', 4, Array, false, 4, 0x1406, 4 ]                   // BOOL_VEC4
 
 };
+
+export function GetUniformSize (type: number): IUniformSize | null
+{
+    const uniformData = glConstMap[type];
+
+    if (uniformData)
+    {
+        return {
+            size: uniformData[4],
+            uboType: uniformData[5],
+            stride: uniformData[6]
+        };
+    }
+
+    return null;
+}
 
 export function GetUniform (gl: WebGL2RenderingContext, type: number): IUniformData | null
 {
