26e24382e77cd8a94c460b60392081d305041857
Richard Davey <rich@photonstorm.com>
2019-11-08 19:13:14 +0000

    Dist files

---
+++ packages/math-matrix4-funcs/dist/LookAt.d.ts
@@ -0,0 +1,4 @@
+import { Matrix4 } from '@phaserjs/math-matrix4';
+import { IVec3 } from '@phaserjs/math-vec3';
+export declare function LookAt(eye: IVec3, center: IVec3, up: IVec3, epsilon?: number): Matrix4;
+//# sourceMappingURL=LookAt.d.ts.map
---
+++ packages/math-matrix4-funcs/dist/TargetTo.d.ts
@@ -0,0 +1,4 @@
+import { Matrix4 } from '@phaserjs/math-matrix4';
+import { IVec3 } from '@phaserjs/math-vec3';
+export declare function TargetTo(eye: IVec3, target: IVec3, up: IVec3, epsilon?: number): Matrix4;
+//# sourceMappingURL=TargetTo.d.ts.map
