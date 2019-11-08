0b13b0bc2e7b6cab101ee4045865278726898788
Richard Davey <rich@photonstorm.com>
2019-11-08 18:40:32 +0000

    New Invert

--- packages/math-matrix4-funcs/dist/Invert.d.ts
+++ packages/math-matrix4-funcs/dist/Invert.d.ts
@@ -1,3 +1,3 @@
 import { IMatrix4, Matrix4 } from '@phaserjs/math-matrix4';
-export declare function Invert(src: IMatrix4): Matrix4;
+export declare function Invert(src: IMatrix4): Matrix4 | null;
 //# sourceMappingURL=Invert.d.ts.map
