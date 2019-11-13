d38400c80e619a40577699a96632197fd1346386
Richard Davey <rich@photonstorm.com>
2019-11-13 16:46:02 +0000

    Optimized ITRS

--- packages/math-matrix2d/src/ITRS.ts
+++ packages/math-matrix2d/src/ITRS.ts
@@ -4,15 +4,22 @@ import { Matrix2D } from './Matrix2D';
 
 export function ITRS (target: Matrix2D, x: number, y: number, angle: number, scaleX: number, scaleY: number): Matrix2D
 {
-    const sin = Math.sin(angle);
-    const cos = Math.cos(angle);
+    if (angle === 0)
+    {
+        return target.set(1, 0, 0, 1, x, y);
+    }
+    else
+    {
+        const sin = Math.sin(angle);
+        const cos = Math.cos(angle);
 
-    return target.set(
-        cos * scaleX,
-        sin * scaleX,
-        -sin * scaleY,
-        cos * scaleY,
-        x,
-        y
-    );
+        return target.set(
+            cos * scaleX,
+            sin * scaleX,
+            -sin * scaleY,
+            cos * scaleY,
+            x,
+            y
+        );
+    }
 }
