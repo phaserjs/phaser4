4f2f9a6a73bf431089e8c9e923253878b8339eef
Richard Davey <rich@photonstorm.com>
2020-02-13 16:35:42 +0000

    Fixed vars

--- packages/math-matrix4-funcs/src/Ortho.ts
+++ packages/math-matrix4-funcs/src/Ortho.ts
@@ -6,12 +6,12 @@ export function Ortho (left: number, right: number, bottom: number, top: number,
     const bottomTop: number = 1 / (bottom - top);
     const nearFar: number = 1 / (near - far);
   
-    const m00: number = -2 * lr;
-    const m11: number = -2 * bt;
-    const m22: number = 2 * nf;
-    const m30: number = (left + right) * lr;
-    const m31: number = (top + bottom) * bt;
-    const m32: number = (far + near) * nf;
+    const m00: number = -2 * leftRight;
+    const m11: number = -2 * bottomTop;
+    const m22: number = 2 * nearFar;
+    const m30: number = (left + right) * leftRight;
+    const m31: number = (top + bottom) * bottomTop;
+    const m32: number = (far + near) * nearFar;
 
     return new Matrix4(m00, 0, 0, 0, 0, m11, 0, 0, 0, 0, m22, 0, m30, m31, m32, 1);
 }
